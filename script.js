b779dd8436d22b5f63b34672cb6a1389b0ac6837
Lari Oliveira <145786541+morena08012008@users.noreply.github.com>
2025-09-10 20:58:27 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaprincipal=document.querySelector(".caixa-principal");
+const caixaperguntas=document.querySelector(".caixa-perguntas");
+const caixaalternativas=document.querySelector("caixa-alternativas");
+const caixaresultado=document.querySelector(".caixa-resultado");
+const textoresultado=document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+enunciado:"assim que saiu do colegio voce se depara com uma nova tecnologia, um chat que consegue responder todas as duvidas que uma pessoa pode ter, ele também gera imagens e áudios. qual é o seu primeiro pensamento?",
+alternativas:[
+{
+    texto: "isso é assustador!",
+afirmacao:"No inicio, eu ficaria com medo do que essa tecnologia pode fazer."
+
+},
+{
+    
+}
+]
+
+    }
+]
